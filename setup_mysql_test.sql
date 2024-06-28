2ef02a286c22dd6fce1669c8e510e01729f4d9f1
Bbr011 <khadraouih7@gmail.com>
2024-06-28 09:16:35 -0700

    MySQL setup test

---
+++ setup_mysql_test.sql
@@ -0,0 +1,20 @@
+-- Create a MySQL server
+-- Database hbnb_test_db
+-- User hbnb_test with password hbnb_test_pwd in localhost
+-- Grant all privileges for hbnb_test on hbnb_test_db
+-- Grant SELECT privilege for hbnb_test on performance_schema
+
+-- CREATE database hbnb_test_db
+CREATE DATATBASE IF NOT EXISTS hbnb_test_db;
+
+-- CREATE user hbnb_test
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+
+-- GRANT all privileges to hbnb_test on hbnb_test_db
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+
+-- GRANT SELECT privilage to hbnb_test on performance_schema
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+
+-- FLUSH so everything gets applies
+FLUSH PRIVILEGES;
