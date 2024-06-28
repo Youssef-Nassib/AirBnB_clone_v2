648419f2a116800e0606117ff4338f9c28536d59
Bbr011 <khadraouih7@gmail.com>
2024-06-28 09:20:17 -0700

    MySQL setup test

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -5,7 +5,7 @@
 -- Grant SELECT privilege for hbnb_test on performance_schema
 
 -- CREATE database hbnb_test_db
-CREATE DATATBASE IF NOT EXISTS hbnb_test_db;
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 
 -- CREATE user hbnb_test
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
