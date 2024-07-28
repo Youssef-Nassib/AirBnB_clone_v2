13fbd06c38336fec4e86af9e82d372446db3a304
Youssef Nassib <wissal2015.nassib@gmail.com>
2024-07-28 16:01:21 -0700

    t

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """ This module contains the function do_pack that generates a .tgz archive
     from the contents of the web_static folder (fabric script) """
+import warnings
 
+warnings.filterwarnings(action='ignore', category=DeprecationWarning)
 from fabric.api import local
 from datetime import datetime
 
