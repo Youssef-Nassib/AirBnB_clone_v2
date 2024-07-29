6f2b850dfacf6b699c591d5062cbceccc63a37ce
Youssef Nassib <wissal2015.nassib@gmail.com>
2024-07-28 17:26:07 -0700

    clean

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,30 +1,36 @@
 #!/usr/bin/python3
 # Fabfile to delete out-of-date archives.
 import os
-from fabric.api import *
+from fabric.api import env, local, run, lcd, cd
 
-env.hosts = ["18.234.249.140", "3.80.18.45"]
-
+env.hosts = ["104.196.168.90", "35.196.46.172"]
 
 def do_clean(number=0):
     """Delete out-of-date archives.
 
     Args:
         number (int): The number of archives to keep.
-
-    If number is 0 or 1, keeps only the most recent archive. If
-    number is 2, keeps the most and second-most recent archives,
-    etc.
     """
     number = 1 if int(number) == 0 else int(number)
 
-    archives = sorted(os.listdir("versions"))
-    [archives.pop() for i in range(number)]
-    with lcd("versions"):
-        [local("rm ./{}".format(a)) for a in archives]
+    # Local archives cleaning
+    if os.path.exists("versions"):
+        archives = sorted(
+            [f for f in os.listdir("versions") if f.endswith(".tgz")],
+            key=lambda x: os.path.getmtime(os.path.join("versions", x)),
+            reverse=True
+        )
+        # Keep only the most recent 'number' archives
+        archives_to_delete = archives[number:]
+        with lcd("versions"):
+            for archive in archives_to_delete:
+                local("rm ./{}".format(archive))
 
-    with cd("/data/web_static/releases"):
-        archives = run("ls -tr").split()
-        archives = [a for a in archives if "web_static_" in a]
-        [archives.pop() for i in range(number)]
-        [run("rm -rf ./{}".format(a)) for a in archives]
+    # Remote archives cleaning
+    for host in env.hosts:
+        with cd("/data/web_static/releases"):
+            archives = run("ls -tr | grep 'web_static_'").split()
+            # Keep only the most recent 'number' archives
+            archives_to_delete = archives[:-number]
+            for archive in archives_to_delete:
+                run("rm -rf ./{}".format(archive))
