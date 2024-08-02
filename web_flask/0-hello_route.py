166102c742d8b45e44194284b563380f0faf29cb
Youssef Nassib <wissal2015.nassib@gmail.com>
2024-08-02 12:57:28 -0700

    hello flask

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
 
-""" this how we start aflask application"""
+"""
+this how we start a Flask application
+"""
 
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
-
-
-
-
 def index():
+    """Return a simple greeting."""
     return "Hello HBNB!"
 
-
-""" this where our application should be listenning"""
+"""
+This is where our application should be listening.
+"""
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
