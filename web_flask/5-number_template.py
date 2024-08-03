9cbe856d89d8abe6b20fbf3722a55c1c29143191
Youssef-Nassib <wissal2015.nassib@gmail.com>
2024-08-03 21:23:48 +0100

    web framework

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -41,7 +41,7 @@ def number(n):
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def display_HTML(n):
-    return render_template('templates/5-number.html')
+    return render_template("5-number.html", n=n)
 
 
 if __name__ == "__main__":
