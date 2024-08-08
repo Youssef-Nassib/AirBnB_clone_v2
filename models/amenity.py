c375d3ab423c0e1806ecb7838740d572f6837f8a
Bbr011 <kahdraouih7@gmail.com>
2024-08-08 06:55:21 -0700

    DBStorage - Amenity... and BOOM!

--- models/amenity.py
+++ models/amenity.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
 """amenity module"""
-from models.base_model import BaseModel
+import os
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
-class Amenity(BaseModel):
-    """Amenity objects"""
+class Amenity(BaseModel, Base):
+    """amenityclass"""
+    __tablename__ = 'amenities'
 
-    name = ""
+    name = Column(
+        String(128), nullable=False
+    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
