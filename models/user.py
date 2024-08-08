84c5a82aa2a9c27cd89d8bbd7dbb0eb9138d47ae
Bbr011 <kahdraouih7@gmail.com>
2024-08-08 06:22:54 -0700

    DBStorage - User

--- models/user.py
+++ models/user.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
-"""This module defines a class User"""
+"""module class User"""
 import os
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
-from models.base_model import BaseModel, Base
-
 
 class User(BaseModel, Base):
-    """This class defines a user by various attributes"""
+    """class that defines a user by it attributes"""
     __tablename__ = 'users'
     email = Column(
         String(128), nullable=False
