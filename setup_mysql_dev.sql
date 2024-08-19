14a14c51847d56be8837a2f50ed787e6366c215e
YoussefNassib <wissal2015.nassib@gmail.com>
2024-08-19 18:43:23 +0000

    List of states

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,20 +1,19 @@
---   Creates a MySQL server
---   Database hbnb_dev_db
---   User hbnb_dev with password hbnb_dev_pwd in localhost
---   Grant all privileges for hbnb_dev on hbnb_dev_db
---   Grant SELECT privilege for hbnb_dev on performance
+-- Creates a MySQL server with:
+--   Database hbnb_dev_db.
+--   User hbnb_dev with password hbnb_dev_pwd in localhost.
+--   Grants all privileges for hbnb_dev on hbnb_dev_db.
+--   Grants SELECT privilege for hbnb_dev on performance_schema.
 
--- CRAETE database hbnb_dev_db
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-
--- CREATE new user hbnb_dev
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-
--- GRANT privileges to hbnb_dev
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
-
--- GARNT SELECT privilege to hbnb_dev on performance_schema database
-GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
-
--- FLUSH so everything gets applied
+CREATE USER
+    IF NOT EXISTS 'hbnb_dev'@'localhost'
+    IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES
+   ON `hbnb_dev_db`.*
+   TO 'hbnb_dev'@'localhost'
+   IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT SELECT
+   ON `performance_schema`.*
+   TO 'hbnb_dev'@'localhost'
+   IDENTIFIED BY 'hbnb_dev_pwd';
 FLUSH PRIVILEGES;
