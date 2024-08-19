28f22c4f3a525618b7dcabbfa728b8a046f10bd2
Youssef-Nassib <wissal2015.nassib@gmail.com>
2024-08-19 18:34:21 +0100

    List of states

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -11,6 +11,10 @@ from flask import render_template
 
 app = Flask(__name__)
 
+@app.teardown_appcontext
+def teardown(exception):
+    """Remove the current SQLAlchemy session."""
+    storage.close()
 
 @app.route("/states_list", strict_slashes=False)
 def states_list():
@@ -22,11 +26,6 @@ def states_list():
     return render_template("7-states_list.html", states=states)
 
 
-@app.teardown_appcontext
-def teardown(exc):
-    """Remove the current SQLAlchemy session."""
-    storage.close()
-
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", port=5000)
